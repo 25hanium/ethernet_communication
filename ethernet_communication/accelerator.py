05baa4e1d2f2a1434e61809c0bca9570f7c0bcfd
serafi1204 <zlakalzn@gmail.com>
2025-07-05 00:15:46 +0900

    s

--- ethernet_communication/accelerator.py
+++ ethernet_communication/accelerator.py
@@ -7,19 +7,19 @@ from .model import model
 from .ethernet import Ethernet
 
 class Accelerator(Ethernet):
-  def __init__(self, HOST, log=False, tag='', model=model(), input_shape=(3, 224, 224)):  
-    super().__init__(HOST, log, tag)
-    self.model = model
-    self.input_shape = input_shape
-    self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    self.path = 'image.png'
-    # setup
-    self.model.to(self.device)
-    self.model.eval()
-    self.transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+    def __init__(self, HOST, log=False, tag='', model=model(), input_shape=(3, 224, 224)):  
+        super().__init__(HOST, log, tag)
+        self.model = model
+        self.input_shape = input_shape
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.path = 'image.png'
+        # setup
+        self.model.to(self.device)
+        self.model.eval()
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
 
     def __call__(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
--- main_accelerator.py
+++ main_accelerator.py
@@ -1,6 +1,6 @@
 import ethernet_communication as ec
 
-IP = '127.0.0.1:5000'
+IP = '169.254.51.93:5000'
 
 acc = ec.Accelerator(IP, True)
 res = acc()
