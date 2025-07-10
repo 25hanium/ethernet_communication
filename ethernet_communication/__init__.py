d324de1031d0cd080b5f947e2972a68373c0b526
serafi1204 <122208990+serafi1204@users.noreply.github.com>
2025-07-10 23:11:42 +0900

    Update __init__.py

--- ethernet_communication/__init__.py
+++ ethernet_communication/__init__.py
@@ -1,4 +1,7 @@
-from .accelerator import Accelerator
-from .host import Host
-
-__all__ = ['Accelerator', 'Host']
+try:
+    from .accelerator import Accelerator
+    from .host import Host
+    __all__ = ['Accelerator', 'Host']
+except ImportError:
+    from .host import Host
+    __all__ = ['Host']
