1077acc063fcd260352693e4500fb3dce5d93440
serafi1204 <122208990+serafi1204@users.noreply.github.com>
2025-07-12 00:00:25 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ethernet_communication', 
-    version='0.9.4',
+    version='0.9.5',
     author='None',
     author_email='None',
     description='CNN communication library based on ethernet.',
